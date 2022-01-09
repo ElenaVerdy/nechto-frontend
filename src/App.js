497b4054121853c345c298ec6e205397cc1f100b
Sakhyanova Elena <e.sakhyanova@sports.ru>
2022-01-09 16:47:08 +0300

    Сетап роутера и сасс

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.sass';
+import './styles/typography.sass';
+import {
+  BrowserRouter as Router,
+  Routes ,
+  Route,
+  NavLink
+} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="App__menu">
+          <NavLink to="/" className="App__menu-item">Рейтинг</NavLink>
+          <NavLink to="/new" className="App__menu-item">Записать результат</NavLink>
+          <NavLink to="/games" className="App__menu-item">Все игры</NavLink>
+        </div>
+
+        <Routes>
+          <Route exact path="/" element={<div>Рейтинг</div>} />
+          <Route path="/new" element={<div>Новая игра</div>} />
+          <Route path="/games" element={<div>Все игры</div>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/AppHeader.js
@@ -0,0 +1,17 @@
+import '../styles/typography.sass';
+
+
+function App() {
+  return (
+    <header className="App-header text-header-3">
+        <div className="App-header text-header-3">
+            Нечто
+        </div>
+        <div className='App-header-menu-button'>
+            Меню
+        </div>
+    </header>
+  );
+}
+
+export default App;
